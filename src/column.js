577c29f7c28cce53dcf783707761fb30e55de672
Wellington Cordeiro <wellington@wellingtoncordeiro.com>
2016-01-25 02:07:37 -0700

    Correct wrapping behavior of column and add 'none' column type from Lost 6.7.0
    Signed-off-by: Wellington Cordeiro <wellington@wellingtoncordeiro.com>

--- src/column.js
+++ src/column.js
@@ -11,48 +11,83 @@ export default function column(columnVal, cycle=CYCLE, gutter=GUTTER, flex=FLEX)
     cycleVal = cycle;
   }
 
-  if (flex === 'flex') {
-    style = addFlex(flex, style);
+  if (columnVal !== 'none') {
+    if (flex === 'flex') {
+      style = addFlex(flex, style);
 
-    if (cycle !== 0) {
+      if (cycle !== 0) {
+        style[`&:nth-child(${cycleVal}n)`] = {
+          marginRight: '0'
+        };
+      }
+
+      style['&:last-child'] = {
+        marginRight: '0'
+      };
+
+
+      style['&:nth-child(n)'] = {
+        marginRight: gutter
+      };
+    } else {
+      if (cycle !== 0) {
+        style[`&:nth-child(${cycleVal}n + 1)`] = {
+          clear: 'left'
+        };
+
+        style[`&:nth-child(${cycleVal}n)`] = {
+          marginRight: '0'
+        };
+
+      }
+
       style[`&:nth-child(${cycleVal}n)`] = {
+        float: 'right'
+      };
+
+      style['&:nth-child(n)'] = {
+        float: 'left',
+        marginRight: gutter,
+        clear: 'none'
+      };
+
+      style['&:last-child'] = {
         marginRight: '0'
       };
     }
 
-    style['&:last-child'] = {
-      marginRight: '0'
-    };
-
-
-    style['&:nth-child(n)'] = {
-      marginRight: gutter
-    };
+    style.width = calcDimension(columnVal, gutter);
   } else {
-    if (cycle !== 0) {
-      style[`&:nth-child(${cycleVal}n + 1)`] = {
-        clear: 'left'
-      };
+    style.width = 'auto';
 
-      style[`&:nth-child(${cycleVal}n)`] = {
-        marginRight: '0'
-      };
+    style['&:nth-child(1n)'] = {
+      float: 'none',
+      clear: 'none',
+      marginRight: '0',
+      width: 'auto'
+    };
 
-    }
-
-    style['&:last-child'] = {
-      marginRight: '0'
+    style['&:nth-child(1n + 1)'] = {
+      float: 'none',
+      clear: 'none',
+      marginRight: '0',
+      width: 'auto'
     };
 
     style['&:nth-child(n)'] = {
-      float: 'left',
-      marginRight: gutter,
-      clear: 'none'
+      float: 'none',
+      clear: 'none',
+      marginRight: '0',
+      width: 'auto'
     };
 
+    style['&:last-child'] = {
+      float: 'none',
+      clear: 'none',
+      marginRight: '0',
+      width: 'auto'
+    };
   }
-
-  style.width = calcDimension(columnVal, gutter);
 
   return style;
 }
