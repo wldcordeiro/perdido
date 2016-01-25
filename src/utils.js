e6b0926512a7ab7aa1b25bc968c6fba968d2cffb
Wellington Cordeiro <wellington@wellingtoncordeiro.com>
2016-01-25 03:38:26 -0700

    Add align method. Change utils.clearFix and utils.edit to exported consts instead of functions.

--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,19 @@
-export function clearFix() {
-  return {
-    '*zoom': '1',
-    '&:after': {
-      content: '""',
-      display: 'table',
-      clear: 'both'
-    },
+export const clearFix = {
+  '*zoom': '1',
+  '&:after': {
+    content: '""',
+    display: 'table',
+    clear: 'both'
+  },
 
-    '&:before': {
-      content: '""',
-      display: 'table'
-    }
-  };
-}
+  '&:before': {
+    content: '""',
+    display: 'table'
+  }
+};
 
-export function edit() {
-  return {
-    '&*:not(input):not(textarea):not(select)': {
-      backgroundColor: 'rgba(0, 0, 255, 0.1)'
-    }
-  };
-}
+export const edit = {
+  '&*:not(input):not(textarea):not(select)': {
+    backgroundColor: 'rgba(0, 0, 255, 0.1)'
+  }
+};
